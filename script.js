fa9921a0f454454866edffbbc1608e787c83540e
rsgerrish <rbn.sills@gmail.com>
2022-04-09 21:44:04 -0400

    Complete UI and Add Additional Functionality

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let playCount = 0;
+let scoreComputer = 0;
+let scorePlayer = 0;
+
 function computerPlay() {
   let i = Math.floor(Math.random() * 3);
 
@@ -25,31 +29,47 @@ function playGame(playerSelection, computerSelection) {
   } else {
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
+    playCount += 1;
+
+    const computerScore = document.querySelector('#computer-score');
+    const playerScore = document.querySelector('#player-score');
     
     if (playerSelection === "rock") {
       if (computerSelection === "rock") {
         return "Tie! Both players chose " + playerSelection;
       } else if (computerSelection === "paper") {
+        scoreComputer += 1;
+        computerScore.textContent = "Computer Score: \n" + scoreComputer;
         return "You Lose! " + capitalize(computerSelection) + " beats " + playerSelection;
       } else if (computerSelection === "scissors") {
+        scorePlayer += 1;
+        playerScore.textContent = "Player Score: \n" + scorePlayer;
         return "You Win! " + capitalize(playerSelection) + " crushes " + computerSelection;
       } else {
         return "Error! Instructions unclear";
       }
     } else if (playerSelection === "paper") {
       if (computerSelection === "rock") {
+        scorePlayer += 1;
+        playerScore.textContent = "Player Score: \n" + scorePlayer;
         return "You Win! " + capitalize(playerSelection) + " covers " + computerSelection;
       } else if (computerSelection === "paper") {
         return "Tie! Both players chose " + playerSelection;
       } else if (computerSelection === "scissors") {
+        scoreComputer += 1;
+        computerScore.textContent = "Computer Score: \n" + scoreComputer;
         return "You Lose! " + capitalize(computerSelection) + " beats " + playerSelection;
       } else {
         return "Error! Instructions unclear";
       }
     } else if (playerSelection === "scissors") {
       if (computerSelection === "rock") {
+        scoreComputer += 1;
+        computerScore.textContent = "Computer Score: \n" + scoreComputer;
         return "You Lose! " + capitalize(computerSelection) + " beats " + playerSelection;
       } else if (computerSelection === "paper") {
+        scorePlayer += 1;
+        playerScore.textContent = "Player Score: \n" + scorePlayer;
         return "You Win! " + capitalize(playerSelection) + " shreds " + computerSelection;
       } else if (computerSelection === "scissors") {
         return "Tie! Both players chose " + playerSelection;
@@ -62,20 +82,86 @@ function playGame(playerSelection, computerSelection) {
   }
 }
 
-function game() {
-  let brk = false;
+function drawBoard() {
+  const instructDiv = document.createElement('div');
+  const btnContainer = document.createElement('div');
+  const scorePlayerDiv = document.createElement('div');
+  const scoreComputerDiv = document.createElement('div');
+  const btnRock = document.createElement('div');
+  const btnPaper = document.createElement('div');
+  const btnScissors = document.createElement('div');
+  const resultsDiv = document.createElement('div');
+  const winnerDiv = document.createElement('div');
+  const container = document.querySelector('body');
 
-  do {
-    let userInput1 = prompt("Please make your choice. Paper, rock or scissors?");
+  instructDiv.textContent = "Please make your choice...";
 
-    let userInput2 = confirm(playGame(userInput1, computerPlay()) + ". Would you like to play again?");
+  btnContainer.classList.add('btnContainer');
 
-    if (userInput2) {
-      brk = false;
-    } else {
-      brk = true;
+  scorePlayerDiv.textContent = "Player Score: \n0";
+  scorePlayerDiv.classList.add('score-card');
+  scorePlayerDiv.setAttribute('id', 'player-score');
+
+  scoreComputerDiv.textContent = "Computer Score: \n0";
+  scoreComputerDiv.classList.add('score-card');
+  scoreComputerDiv.setAttribute('id', 'computer-score');
+
+  btnRock.textContent = "Rock";
+  btnRock.classList.add('btn');
+
+  btnPaper.textContent = "Paper";
+  btnPaper.classList.add('btn');
+
+  btnScissors.textContent = "Scissors";
+  btnScissors.classList.add('btn');
+
+  instructDiv.classList.add('instructions');
+  resultsDiv.classList.add('results');
+  winnerDiv.classList.add('winner');
+
+  container.appendChild(instructDiv);
+  container.appendChild(btnContainer);
+  btnContainer.appendChild(scorePlayerDiv);
+  btnContainer.appendChild(btnRock);
+  btnContainer.appendChild(btnPaper);
+  btnContainer.appendChild(btnScissors);
+  btnContainer.appendChild(scoreComputerDiv);
+  container.appendChild(resultsDiv);
+  container.appendChild(winnerDiv);
+
+  btnRock.addEventListener('click', function () {
+    resultsDiv.textContent = playGame('rock', computerPlay());
+
+    if (playCount >= 5) {
+      if(scorePlayer > scoreComputer) {winnerDiv.textContent = `You Won It All! ${scorePlayer} to ${scoreComputer}`}
+      else if (scorePlayer < scoreComputer) {winnerDiv.textContent = `You Were Beat By a Dumb Computer LUL ${scorePlayer} to ${scoreComputer}`}
+      else if (scorePlayer === scoreComputer) {winnerDiv.textContent = `Its an Overall Tie! Womp Womp ${scorePlayer} to ${scoreComputer}`}
+      else {winnerDiv.textContent = "ERROR ERROR ERROR"}
     }
-  } while (brk === false);
+  });
+
+  btnPaper.addEventListener('click', function () {
+    
+    resultsDiv.textContent = playGame('paper', computerPlay());
+
+    if (playCount >= 5) {
+      if(scorePlayer > scoreComputer) {winnerDiv.textContent = `You Won It All! ${scorePlayer} to ${scoreComputer}`}
+      else if (scorePlayer < scoreComputer) {winnerDiv.textContent = `You Were Beat By a Dumb Computer LUL ${scorePlayer} to ${scoreComputer}`}
+      else if (scorePlayer === scoreComputer) {winnerDiv.textContent = `Its an Overall Tie! Womp Womp ${scorePlayer} to ${scoreComputer}`}
+      else {winnerDiv.textContent = "ERROR ERROR ERROR"}
+    }
+  });
+
+  btnScissors.addEventListener('click', function () {
+    resultsDiv.textContent = playGame('scissors', computerPlay());
+
+    if (playCount >= 5) {
+      if(scorePlayer > scoreComputer) {winnerDiv.textContent = `You Won It All! ${scorePlayer} to ${scoreComputer}`}
+      else if (scorePlayer < scoreComputer) {winnerDiv.textContent = `You Were Beat By a Dumb Computer LUL ${scorePlayer} to ${scoreComputer}`}
+      else if (scorePlayer === scoreComputer) {winnerDiv.textContent = `Its an Overall Tie! Womp Womp ${scorePlayer} to ${scoreComputer}`}
+      else {winnerDiv.textContent = "ERROR ERROR ERROR"}
+    }
+  });
 }
 
-game();
+drawBoard();
